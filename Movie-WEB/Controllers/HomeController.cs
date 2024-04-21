a2bf5803d437e8dd72d7182a732c952386cc273a
dogukanyalci <yalcidogukan965@gmail.com>
2024-04-21 15:48:12 +0300

    Update

--- Movie-WEB/Controllers/HomeController.cs
+++ Movie-WEB/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using Movie_Core.Entities.Abstract;
+using Movie_DataAccess.Services.Concrete;
+using Movie_DataAccess.Services.Interface;
 using Movie_WEB.Models;
+using Movie_WEB.Models.ViewModels;
 using System.Diagnostics;
 
 namespace Movie_WEB.Controllers
@@ -7,15 +11,64 @@ namespace Movie_WEB.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IMovieRepository _movieRepository;
+        private readonly ITvSeriesRepository _tvSeriesRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IMovieRepository movieRepository, ITvSeriesRepository tvSeriesRepository)
         {
             _logger = logger;
+            _movieRepository = movieRepository;
+            _tvSeriesRepository = tvSeriesRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var movies = await _movieRepository.GetFilteredListAsync
+                (
+                    select: x => new MovieVM
+                    {
+                        Id = x.Id,
+                        MovieName = x.MovieName,
+                        Description = x.Description,
+                        Director = x.Director,
+                        Genre = x.Genre,
+                        Cast = x.Cast,
+                        Duration = x.Duration,
+                        ReleaseDate = x.ReleaseDate,
+                        ImdbRating = x.ImdbRating,
+                        ImagePath = x.ImagePath,
+                        TrailerPath = x.TrailerPath,
+                    },
+                    where: x => x.Status != Status.Passive,
+                    orderBy: x => x.OrderByDescending(z => z.ImdbRating)
+                );
+
+            var tvSeries = await _tvSeriesRepository.GetFilteredListAsync
+               (
+                   select: x => new TvSeriesVM
+                   {
+                       Id = x.Id,
+                       TvSeriesName = x.TvSeriesName,
+                       Description = x.Description,
+                       Director = x.Director,
+                       Genre = x.Genre,
+                       Cast = x.Cast,
+                       ReleaseDate = x.ReleaseDate,
+                       ImdbRating = x.ImdbRating,
+                       ImagePath = x.ImagePath,
+                       TrailerPath = x.TrailerPath
+                   },
+                   where: x => x.Status != Status.Passive,
+                   orderBy: x => x.OrderByDescending(z => z.ImdbRating)
+               );
+
+            var movieSeries = new MovieSeriesVM
+            {
+                Movies = movies,
+                TvSeries = tvSeries
+            };
+
+            return View(movieSeries);
         }
 
         public IActionResult Privacy()
---
+++ Movie-WEB/Models/ViewModels/MovieSeriesVM.cs
@@ -0,0 +1,8 @@
+﻿namespace Movie_WEB.Models.ViewModels
+{
+    public class MovieSeriesVM
+    {
+        public List<MovieVM> Movies { get; set; }
+        public List<TvSeriesVM> TvSeries { get; set; }
+    }
+}
