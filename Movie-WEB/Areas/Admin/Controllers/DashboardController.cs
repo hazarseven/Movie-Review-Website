9379c9f5a5472f9ea32e370bcfae834db6f2f16f
Hazar Seven <hazar.seven@gazi.edu.tr>
2024-04-24 16:33:22 +0300

    update-123

--- Movie-WEB/Areas/Admin/Controllers/DashboardController.cs
+++ Movie-WEB/Areas/Admin/Controllers/DashboardController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Movie_Core.DTO_s.MovieDTO;
+using Movie_Core.DTO_s.TvSeriesDTO;
 using Movie_Core.Entities.Abstract;
+using Movie_Core.Entities.Concrete;
 using Movie_DataAccess.Services.Interface;
 using Movie_WEB.Models.ViewModels;
 
@@ -12,11 +16,13 @@ namespace Movie_WEB.Areas.Admin.Controllers
     {
 		private readonly IMovieRepository _movieRepository;
 		private readonly ITvSeriesRepository _tvSeriesRepository;
+		private readonly IMapper _mapper;
 
-		public DashboardController(IMovieRepository movieRepository, ITvSeriesRepository tvSeriesRepository)
+		public DashboardController(IMovieRepository movieRepository, ITvSeriesRepository tvSeriesRepository, IMapper mapper)
         {
 			_movieRepository = movieRepository;
 			_tvSeriesRepository = tvSeriesRepository;
+			_mapper = mapper;
 		}
         public async Task<IActionResult> Index()
 		{
@@ -66,6 +72,182 @@ namespace Movie_WEB.Areas.Admin.Controllers
 			};
 
 			return View(movieSeries);
+		}
+
+		[Authorize(Roles = "editor")]
+		public IActionResult AddMovie()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[Authorize(Roles = "editor")]
+		public async Task<IActionResult> AddMovie(AddMovieDTO model)
+		{
+			if (ModelState.IsValid)
+			{
+				var movie = _mapper.Map<Movie>(model);
+				await _movieRepository.AddAsync(movie);
+				TempData["Success"] = "Movie added successfully";
+				return RedirectToAction("Index");
+			}
+			TempData["Error"] = "Please follow the rules below!";
+			return View(model);
+		}
+
+		[Authorize(Roles = "editor")]
+
+		public async Task<IActionResult> UpdateMovie(int id)
+		{
+			if (id > 0)
+			{
+				var movie = await _movieRepository.GetByIdAsync(id);
+				if (movie != null)
+				{
+					var model = _mapper.Map<UpdateMovieDTO>(movie);
+					return View(model);
+				}
+			}
+			TempData["Error"] = "Movie not found!";
+			return RedirectToAction("Index");
+		}
+
+		[HttpPost]
+		[Authorize(Roles = "editor")]
+
+		public async Task<IActionResult> UpdateMovie(UpdateMovieDTO model)
+		{
+			if (ModelState.IsValid)
+			{
+				var movie = _mapper.Map<Movie>(model);
+				await _movieRepository.UpdateAsync(movie);
+				TempData["Success"] = "Movie updated successfully";
+				return RedirectToAction("Index");
+			}
+			TempData["Error"] = "Please follow the rules below!";
+			return View(model);
+		}
+
+		[Authorize(Roles = "editor")]
+		public async Task<IActionResult> DeleteMovie(int id)
+		{
+			if (id > 0)
+			{
+				var movie = await _movieRepository.GetByIdAsync(id);
+				if (movie != null)
+				{
+					await _movieRepository.DeleteAsync(movie);
+					TempData["Success"] = "Movie deleted successfully";
+					return RedirectToAction("Index");
+				}
+			}
+			TempData["Error"] = "Movie not found!";
+			return RedirectToAction("Index");
+		}
+
+		[AllowAnonymous]
+		public async Task<IActionResult> MovieDetail(int id)
+		{
+			if (id > 0)
+			{
+				var movie = await _movieRepository.GetByIdAsync(id);
+				if (movie != null)
+				{
+					var model = _mapper.Map<MovieDetailDTO>(movie);
+					return View(model);
+				}
+			}
+			TempData["Error"] = "Movie not found!";
+			return RedirectToAction("Index");
+		}
+
+		[Authorize(Roles = "editor")]
+		public IActionResult AddTvSeries()
+		{
+			return View();
+		}
+
+
+		[HttpPost]
+		[Authorize(Roles = "editor")]
+		public async Task<IActionResult> AddTvSeries(AddTvSeriesDTO model)
+		{
+			if (ModelState.IsValid)
+			{
+				var tvSeries = _mapper.Map<TvSeries>(model);
+				await _tvSeriesRepository.AddAsync(tvSeries);
+				TempData["Success"] = "Tv Series added successfully";
+				return RedirectToAction("Index");
+			}
+			TempData["Error"] = "Please follow the rules below!";
+			return View(model);
+		}
+
+
+		[Authorize(Roles = "editor")]
+		public async Task<IActionResult> UpdateTvSeries(int id)
+		{
+			var tvSeries = await
+				_tvSeriesRepository.GetByIdAsync(id);
+			if (tvSeries != null)
+			{
+				var model = _mapper.Map<UpdateTvSeriesDTO>(tvSeries);
+				return View(model);
+			}
+			TempData["Error"] = "TvSeries not found";
+			return RedirectToAction("Index");
+		}
+
+
+
+
+		[HttpPost]
+		[Authorize(Roles = "editor")]
+		public async Task<IActionResult> UpdateTvSeries(UpdateTvSeriesDTO model)
+		{
+			if (ModelState.IsValid)
+			{
+				var tvSeries = _mapper.Map<TvSeries>(model);
+				await _tvSeriesRepository.UpdateAsync(tvSeries);
+				TempData["Success"] = "Tv Series updated successfully";
+				return RedirectToAction("Index");
+			}
+			TempData["Error"] = "Please follow the rules below!";
+			return View(model);
+		}
+
+
+		[Authorize(Roles = "editor")]
+		public async Task<IActionResult> DeleteTvSeries(int id)
+		{
+			if (id > 0)
+			{
+				var tvSeries = await _tvSeriesRepository.GetByIdAsync(id);
+				if (tvSeries != null)
+				{
+					await _tvSeriesRepository.DeleteAsync(tvSeries);
+					TempData["Success"] = "Tv Series deleted successfully";
+					return RedirectToAction("Index");
+				}
+			}
+			TempData["Error"] = "Tv Series not found";
+			return RedirectToAction("Index");
+		}
+
+		[AllowAnonymous]
+		public async Task<IActionResult> TvSeriesDetail(int id)
+		{
+			if (id > 0)
+			{
+				var tvSeries = await _tvSeriesRepository.GetByIdAsync(id);
+				if (tvSeries != null)
+				{
+					var model = _mapper.Map<TvSeriesDetailDTO>(tvSeries);
+					return View(model);
+				}
+			}
+			TempData["Error"] = "Tv Series not found";
+			return RedirectToAction("Index");
 		}
 	}
 }
--- Movie-WEB/wwwroot/js/site.js
+++ Movie-WEB/wwwroot/js/site.js
@@ -2,6 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $('.myTable').DataTable({
+        responsive: true,
+        scrollX: true
+    });
+
+    $('.dt-search label').html("Arama: ")
+
+});
+
 setTimeout(() => {
     $('.notification').fadeOut("slow")
 }, 6000);
